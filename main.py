25a7377f4d6f68f4892d3fa4cccc5eb384a4734c
Moon-Raven <vukant@gmail.com>
2022-01-04 16:33:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def main():
+    print(f"Arguments count: {len(sys.argv)}")
+    for i, arg in enumerate(sys.argv):
+        print(f"Argument {i:>6}: {arg}")
+
+
+if __name__ == '__main__':
+    main()
