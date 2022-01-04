64e12024bc650221c8bb8357c0be0ae6f699375c
Moon-Raven <vukant@gmail.com>
2022-01-04 17:13:38 +0100

    Add initial argument parsing

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
-import sys
+import argparse
+
+import python.custom.main_custom
+import python.data.main_data
+import python.figure.main_figure
+import python.nu.main_nu
+
+
+def parse_cli_arguments():
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument(
+        'Command',
+        metavar='command',
+        type=str,
+        help='command to run',
+        choices=['data', 'figure', 'nu', 'custom'])
+
+    arg_parser.add_argument('-s', '--system', help='system to analyze')
+
+    arg_parser.add_argument(
+        '-a',
+        '--algorithm',
+        help='algorithm to run',
+        choices=['line', 'region'])
+
+    arg_parser.add_argument(
+        '-p',
+        '--parallel',
+        action='store_true',
+        help='parellelize code execution')
+
+    args = arg_parser.parse_args()
+    return args
 
 
 def main():
-    print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
+    args = parse_cli_arguments()
+
+    if args.Command == 'custom':
+        python.custom.main_custom.main(args)
+    elif args.Command == 'data':
+        python.data.main_data.main(args)
+    elif args.Command == 'figure':
+        python.figure.main_figure.main(args)
+    elif args.Command == 'nu':
+        python.nu.main_nu.main(args)
+    else:
+        raise argparse.ArgumentError(f'Unknown command: {args.Command}')
 
 
 if __name__ == '__main__':
