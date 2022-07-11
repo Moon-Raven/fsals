af8ed88ed60d083acc671a4b3df7d1494fbf96f1
Moon-Raven <vukant@gmail.com>
2022-07-11 22:26:27 +0200

    Update batch script

--- make_figs.sh
+++ make_figs.sh
@@ -3,25 +3,25 @@
 LOGNAME=script_progress.txt
 rm -f $LOGNAME
 
-SYSTEMS=()
-SYSTEMS+=(retarded1)
-SYSTEMS+=(distributed_delay1)
-SYSTEMS+=(semi_infinite_rod)
-SYSTEMS+=(finite_rod)
-SYSTEMS+=(pde_complex_k_sigma)
-SYSTEMS+=(pde_complex_tau_sigma)
-SYSTEMS+=(pde_complex_beta_sigma)
-SYSTEMS+=(telegrapher_x_k)
-SYSTEMS+=(telegrapher_alpha_gamma)
-SYSTEMS+=(telegrapher_standard)
+CONFIGURATIONS=()
+CONFIGURATIONS+=(retarded1)
+CONFIGURATIONS+=(distributed_delay1)
+CONFIGURATIONS+=(semi_infinite_rod)
+CONFIGURATIONS+=(finite_rod)
+CONFIGURATIONS+=(pde_complex_k_sigma)
+CONFIGURATIONS+=(pde_complex_tau_sigma)
+CONFIGURATIONS+=(pde_complex_beta_sigma)
+CONFIGURATIONS+=(telegrapher_x_k)
+CONFIGURATIONS+=(telegrapher_alpha_gamma)
+CONFIGURATIONS+=(telegrapher_standard)
 
 ALGORITHMS=()
 ALGORITHMS+=(line)
 ALGORITHMS+=(region)
 
-for system in ${SYSTEMS[@]}; do
+for configuration in ${CONFIGURATIONS[@]}; do
     for algo in ${ALGORITHMS[@]}; do
-        echo $system $algo >> $LOGNAME
-        python3 main.py -l info -a $algo -s $system figure
+        echo $configuration $algo >> $LOGNAME
+        python3 main.py -l info -a $algo -c $configuration figure
     done
 done
